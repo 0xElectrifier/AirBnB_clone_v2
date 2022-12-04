d8f8d50ae3dc114a1969a6c18b61617c629d11aa
Electrifier-Ubuntu <samchijioke16@gmail.com>
2022-12-04 19:47:11 +0100

    State.py

--- models/state.py
+++ models/state.py
@@ -12,5 +12,5 @@ class State(BaseModel):
 
     def __init__(self, *args, **kwargs):
         """Initialises Amenity"""
-        super().__init(*args, **kwargs)
+        super().__init__(*args, **kwargs)
 
