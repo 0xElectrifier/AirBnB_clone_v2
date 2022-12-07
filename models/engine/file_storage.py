561fa96ba23e982cc323d788e9519dce5bca4bba
Akacha-Ws <samchijioke16@gmai.com>
2022-12-07 01:43:39 -0800

    Changed prototype for .all() method

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -8,7 +8,7 @@ class FileStorage:
     __file_path = 'file.json'
     __objects = {}
 
-    def all(self):
+    def all(self, cls=None):
         """Returns a dictionary of models currently in storage
         if a cls is specified, returns a dictionary of objects of that type.
         Otherwise, returns the __objects dictionary.
