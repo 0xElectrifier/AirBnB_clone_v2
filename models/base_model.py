566c7357e7f98a82aefd5df5da4cd26600bdeeff
Akacha-Ws <samchijioke16@gmai.com>
2022-12-06 17:14:53 -0800

    More mod

--- models/base_model.py
+++ models/base_model.py
@@ -35,7 +35,9 @@ class BaseModel:
                 setattr(self, 'updated_at', datetime.now())
             if 'id' not in kwargs.keys():
                 setattr(self, 'id', str(uuid.uuid4()))
-                storage.new(self)
+
+            storage.new(self)
+
 
     def __str__(self):
         """Returns a string representation of the instance"""
--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -14,7 +14,11 @@ class FileStorage:
 
     def new(self, obj):
         """Adds new object to storage dictionary"""
-        self.all().update({obj.to_dict()['__class__'] + '.' + obj.id: obj})
+        try:
+            self.all().update({obj.to_dict()['__class__'] + '.' + obj.id: obj})
+        except TypeError:
+            self.all().update({obj.to_dict()['__class__'] + '.' + str(obj.id):
+                               obj})
 
     def save(self):
         """Saves storage dictionary to file"""
@@ -45,6 +49,6 @@ class FileStorage:
             with open(FileStorage.__file_path, 'r') as f:
                 temp = json.load(f)
                 for key, val in temp.items():
-                        self.all()[key] = classes[val['__class__']](**val)
+                    self.all()[key] = classes[val['__class__']](**val)
         except FileNotFoundError:
             pass
