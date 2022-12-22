5ea811cdc002c178db40d4d52098dd2c800df03b
0xElectrifier-Ubuntu <samchijioke16@gmail.com>
2022-12-22 14:08:36 +0100

    Server configuration file to setup 'web-static'

---
+++ 0-setup_web_static.sh
@@ -0,0 +1,41 @@
+#!/usr/bin/env bash
+# Script that sets up your web servers for the deployment of 'web_static'
+
+# Check if 'nginx' is installed. If not, install
+res=$(eval dpkg -s nginx | grep "Status: install ok installed*")
+if [ -n $res ]
+then
+	sudo apt-get update;
+	sudo apt-get install nginx;
+fi
+
+mkdir -p /data/web_static/releases/test;
+mkdir -p /data/web_static/shared;
+echo "Testing Nginx configuration" >> /data/web_static/releases/test/index.html;
+link0="/data/web_static/current"
+# Remove symlink $link0 if it exists
+if [ -h $link0 ]
+then
+	rm -rf $link0
+fi
+
+# Create symlink
+ln -s /data/web_static/releases/test $link0;
+
+# Change ownership of /data/ recursively
+sudo chown -R ubuntu:ubuntu /data;
+
+# 'Server' main context to configure server
+echo "server {
+	listen 80;
+	listen [::]:80;
+
+	root /data/web_static/releases/test;
+	index.html
+
+	location /hbnb_static {
+	    alias /data/web_static/current;
+}" > /etc/nginx/sites_available/default
+
+# Restart nginx to reconfigure server
+service nginx restart;
