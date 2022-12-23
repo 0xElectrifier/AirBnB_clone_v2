a5bf302773bd3c3b7ce854d519f4c0c0786c5e80
0xElectrifier-Ubuntu <samchijioke16@gmail.com>
2022-12-24 00:07:13 +0100

    Done with mod

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -3,36 +3,53 @@
 'web_static' folder of the AirBnB Clone repo
 """
 from fabric.api import *
-from datetime import datetime
+from os import path
 import re
 
-
-def do_pack():
-    """Fab script to compress directory
-
-    Return: path to created archive
-    """
-    # Get current time
-    dt = datetime.isoformat(datetime.now())
-    dt = re.split("[-T:.]", dt)
-    archive_name = "./versions/web_static_" + dt[0] + dt[1] + dt[2]
-    archive_name += dt[3] + dt[4] + dt[5] + '.tgz'
-
-    # Create directory versions and compress web_static dir
-    local('mkdir -p versions')
-    result = local("tar -cvzf {} web_static".format(archive_name))
-
-    # Check if archiving failed
-    if (result.failed):
-        return None
-
-    return (archive_name)
+env.hosts = ['52.87.219.245', '18.208.120.8']
+env.user = 'ubuntu'
 
 
 def do_deploy(archive_path):
     """Deploys an archive to my web server
-    
+
     Return:
         False if the file at @archive_path doesn't exist
     """
-    run('scp {} ubu
+
+    try:
+        # Check if @archive_path exists
+        if not path.exists(archive_path):
+            return False
+
+        # Transfer the archive from local to remote
+        put(archive_path, '/tmp/')
+        
+        # Get the name of the file
+        file = re.split("/", archive_path)[-1]
+        tmp_path = ('/tmp/' + file)
+        
+
+        # Get filename without the extension
+        # filename = re.split(".", file)[0]
+        filename = file[:-4]
+        local('echo {}'.format(filename))
+
+        # Uncompress the archive to the remote folder
+
+        dest = "/data/web_static/releases/" + filename
+        run('sudo mkdir -p {}'.format(dest))
+        run('sudo tar -xzf {} -C {}'.format(tmp_path, dest))
+
+        # Delete the archive from the web server
+        run('sudo rm -rf {}'.format(tmp_path))
+
+        # Delete symbolic link and recreate a new one linked to the
+        # new version of code
+        run('sudo rm -rf /data/web_static/current')
+        new_symlink = '/data/web_static/releases/' + filename
+        run('sudo ln -s {} /data/web_static/current'.format(new_symlink))
+    except Exception as e:
+        local('echo Error - {}'.format(e))
+
+    return True
