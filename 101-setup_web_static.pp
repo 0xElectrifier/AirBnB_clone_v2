769d0bcf5858051c03feb3947c65fcbcd2e05616
0xElectrifier-Ubuntu <samchijioke16@gmail.com>
2023-01-01 18:37:11 +0100

    Rewrite file

--- 101-setup_web_static.pp
+++ 101-setup_web_static.pp
@@ -1,6 +1,80 @@
-# setup web server for deployment
+# Setup web server for deployment
 
-exec { 'server setup':
-  command  => 'sudo apt-get -y update && sudo apt-get -y install nginx && sudo service nginx start && sudo mkdir -p /data/web_static/shared/ && sudo mkdir -p /data/web_static/releases/test/ && echo "Holberton School" | sudo tee /data/web_static/releases/test/index.html > /dev/null && sudo ln -sf /data/web_static/releases/test/ /data/web_static/current && sudo chown -R ubuntu:ubuntu /data/ && sudo sed -i \'44i \\n\tlocation /hbnb_static {\n\t\talias /data/web_static/current/;\n\t}\' /etc/nginx/sites-available/default && sudo service nginx restart',
-  provider => shell,
+$html = "<html>
+  <head>
+  </head>
+  <body>
+    Holberton School
+  </body>
+</html>
+
+$var="server {
+	listen 80 default_server;
+	listen [::]:80 default_server;
+
+	location /hbnb_static {
+	    alias /data/web_static/current;
+	    index index.html index.htm;
+	}
+
+	add_header X-Served-By ${hostname};
+
+	rewrite ^/redirect_me https://www.youtube.comwatch?v=QH2-TGU1\wu4 permanent;
+}"
+
+exec { 'Update':
+  path	   => ['/usr/bin', '/sbin', '/bin', '/usr/sbin'],
+  command  => 'sudo apt-get update -y',
+  provider => 'shell',
+  returns  => [0,1],
+} ->
+
+package { 'nginx':
+  ensure => 'present',
+} ->
+
+file { '/data':
+  ensure => 'directory',
+} ->
+
+file { '/data/web_static':
+  ensure => 'directory',
+} ->
+
+file { '/data/web_static/releases':
+  ensure => 'directory',
+} ->
+
+file { '/data/web_static/releases/test':
+  ensure => 'directory',
+} ->
+
+file { '/data/web_static/shared':
+  ensure => 'directory',
+} ->
+
+file { '/data/web_static/releases/test/index.html':
+  ensure  => 'present',
+  content => $html,
+} ->
+
+file { '/data/web_static/current':
+  ensure => 'link',
+  target => '/data/web_static/releases/test',
+  force  => 'yes',
+} ->
+
+exec { 'permissions' :
+  path	  => ['/usr/bin', '/sbin', '/bin', '/usr/sbin', '/usr/local/bin'],
+  command => 'chown -R ubuntu:ubuntu /data/',
+  returns => [0,1],
+} ->
+
+file { '/etc/nginx/sites-available/default':
+  ensure  => 'present',
+  content => $var,
+} ->
+
+service { 'nginx':
+  ensure => 'running',
 }
