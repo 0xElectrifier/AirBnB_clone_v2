481f9ad7dddcb61aeb05d0c398aa6904b32ffb13
0xElectrifier-Ubuntu <samchijioke16@gmail.com>
2023-01-14 22:43:31 +0100

    Update DBStorage

--- models/state.py
+++ models/state.py
@@ -14,6 +14,7 @@ class State(BaseModel, Base):
                       nullable=False)
         cities = relationship('City', backref='state',
                               cascade='all, delete, delete-orphan')
+
     else:
         name = ""
 
