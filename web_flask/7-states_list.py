84740b84b53bdce77e3b7dd032764f5d120ed02f
0xElectrifier-Ubuntu <samchijioke16@gmail.com>
2023-01-15 18:06:03 +0100

    Added a 'teardown' method

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -11,6 +11,10 @@ def states_list():
     states = storage.all('State')
     return render_template('7-states_list.html', states=states)
 
+@app.teardown_appcontext
+def teardown():
+    storage.close()
+
 
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5000)
