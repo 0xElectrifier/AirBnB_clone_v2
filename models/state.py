4585c19068f8afc851e60467b7eca32b17fb91a9
0xElectrifier-Ubuntu <samchijioke16@gmail.com>
2023-01-16 04:26:43 +0100

    Debugging

--- models/state.py
+++ models/state.py
@@ -13,7 +13,7 @@ class State(BaseModel, Base):
     if HBNB_TYPE_STORAGE == 'db':
         name = Column(String(128),
                       nullable=False)
-        cities = relationship('City', backref='state',
+        #cities = relationship('City', backref='state',
                               cascade='all, delete, delete-orphan')
 
     else:
